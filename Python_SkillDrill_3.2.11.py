0b8de83976280187184a432cbaedbe9bcbcce0e9
Laura Lewis <114887886+havgirl@users.noreply.github.com>
2022-10-23 15:56:56 -0400

    Delete Python_SkillDrill_3.2.11.py

--- Python_SkillDrill_3.2.11.py
+++
@@ -1,5 +0,0 @@
-voting_data = [{"county":"Arapahoe" , "registered_voters": 422829} , {"county":"Denver" , "registered_voters": 463353}, {"county":"Jefferson" , "registered_voters": 432438}]
-
-for counties_dict in voting_data:
-    for i in counties_dict.values("county") and j in counties_dict.values(registered_voters):
-        print(f" {i} county has {j: ,} registered voters.")
